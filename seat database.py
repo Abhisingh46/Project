c0c606622384a2148d8e0c281e0c891430d31c4f
Kumar Abhinandan <87109787+Abhisingh46@users.noreply.github.com>
2021-11-30 15:13:17 +0530

    Add files via upload

---
+++ seat database.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Seat Database
+
+
+# In[2]:
+
+
+seating=[]
+
+
+# In[3]:
+
+
+class Seats:
+    def __init__(self,name,gender,age,phone,price):
+        self.name=name
+        self.gender=gender
+        self.age=age
+        self.phone=phone
+        self.price=price
+
+
+# In[4]:
+
+
+booked={}
+tickets_booked=0
+current_income=0
+Total_Income=0
+
+
+# In[ ]:
+
+
+
+
